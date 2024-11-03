1be0746eab23914692df4d7cb7e6f8e5e33758c4
shubham-mehta-002 <mehta.shubham002@gmail.com>
2024-11-04 02:26:29 +0530

    add main process code

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("HELLO")
+const path = require("path")
+const {app,BrowserWindow} = require("electron")
+
+const createBrowser = () =>{
+    const win = new BrowserWindow({
+        height:600,
+        width : 500,
+        webPreferences:{
+            nodeIntegration:true,
+            contextIsolation:true,
+            preload: path.join(__dirname,"renderer.js")
+        }
+    })
+    win.loadFile("index.html")
+}
+
+app.whenReady().then(createBrowser)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
